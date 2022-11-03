56f2f85dd837ecd01fcc0d8d6aebc2dc8194e622
Hamza405 <hamzash863@gmail.com>
2022-11-03 17:30:00 +0200

    Implement register page design

--- src/Pages/Register/RegisterPage.jsx
+++ src/Pages/Register/RegisterPage.jsx
@@ -5,15 +5,17 @@ const RegisterPage = () => {
   return (
     <div className={style.formContainer}>
       <div className={style.formWrapper}>
-        <span className={style.logo}>Chat app</span>
-        <span className={style.title}>Register</span>
-        <input type="text" placeholder="Your name" />
-        <input type="email" placeholder="Your email" />
-        <input type="password" placeholder="Your password" />
-        <input type="file" />
-        <button>Sign up</button>
+        <form className={style.formWrapper}>
+          <span className={style.logo}>Chat app</span>
+          <span className={style.title}>Register</span>
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Your email" />
+          <input type="password" placeholder="Your password" />
+          <input type="file" />
+          <button>Sign up</button>
+          <p>Do you have an account ? login</p>
+        </form>
       </div>
-      <p>Do you have an account ? login</p>
     </div>
   );
 };
