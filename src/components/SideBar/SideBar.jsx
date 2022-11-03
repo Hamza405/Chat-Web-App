8a4c9c9e744992a88319814f54820516aed7be9b
Hamza405 <hamzash863@gmail.com>
2022-11-04 01:20:54 +0200

    Implement search component design

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import Search from "../Search/Search";
 import style from "./SideBarStyle.module.scss";
 
 const SideBar = () => {
   return (
     <div className={style.sidebar}>
       <NavBar />
+      <Search />
     </div>
   );
 };
