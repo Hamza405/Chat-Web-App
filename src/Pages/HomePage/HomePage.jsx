aea8e013ef5b5245738c3b5775c812daa48bdf91
Hamza405 <hamzash863@gmail.com>
2022-11-04 00:43:50 +0200

    init home page

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./HomeStyle.scss";
+
+const HomePage = () => {
+  return (
+    <div className={style.Home}>
+      <div className={style.container}></div>
+    </div>
+  );
+};
+
+export default HomePage;
