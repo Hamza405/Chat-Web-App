22200e796ceeadc5335e4b7900107d33b42b83f8
Hamza405 <hamzash863@gmail.com>
2022-11-04 10:48:11 +0200

    Implement chat top bar design

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import Cam from "../../assets/images/cam.png";
+import Add from "../../assets/images/add.png";
+import More from "../../assets/images/more.png";
 import style from "./ChatStyle.module.scss";
 
 const Chat = () => {
-  return <div className={style.chat}>c</div>;
+  return (
+    <div className={style.chat}>
+      <div className={style.chatInfo}>
+        <span>Hamza</span>
+        <div className={style.chatInfos}>
+          <img src={Cam} alt="chatInfo" />
+          <img src={Add} alt="chatInfo" />
+          <img src={More} alt="chatInfo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
