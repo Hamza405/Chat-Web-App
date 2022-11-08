322c1cf325076fe631e68b4bfa315a4b5e1b756c
Hamza405 <hamzash863@gmail.com>
2022-11-08 13:52:39 +0200

    listen to messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { onSnapshot, doc } from "firebase/firestore";
+import { db } from "../../services/firebase";
+import ChatContext from "../../store/ChatContext";
 import Message from "../Message/Message";
 import style from "./MessagesStyle.module.scss";
 
 const Messages = () => {
+  const { data } = useContext(ChatContext);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
+  console.log(messages);
   return (
     <div className={style.messages}>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((m) => (
+        <Message message={m} />
+      ))}
     </div>
   );
 };
